55c828893b53ac6bfa51af51e6191bc5826e19e1
cityho <81910419+cityho@users.noreply.github.com>
2022-11-25 17:18:25 +0900

    Clean up unused files

--- lyrics/deprecated/collect_data.py
+++ lyrics/deprecated/collect_data.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 from typing import Dict, List
 import time
 from lyricsgenius import Genius
-from lyrics.config import *
+from lyrics.deprecated.config import *
 import requests
 import traceback
 
